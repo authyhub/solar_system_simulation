d48bf59d2eacd3528ab9f160ed2b566352da0f26
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2024-03-24 12:19:43 +0100

    Add more attributes to planet class

--- planet.py
+++ planet.py
@@ -1,12 +1,27 @@
 class Planet:
 
-    def __init__(self, name, x, y, radius, mass, color: tuple = (255, 255, 255)):
+    def __init__(
+        self,
+        name: str,
+        x: float,
+        y: float,
+        radius: float,
+        mass: float,
+        color: tuple = (255, 255, 255),
+    ):
         self.name = name
         self.x = x
         self.y = y
         self.radius = radius
         self.mass = mass
         self.color = color
+
+        self.orbit = []
+        self.u = 0  # x component of velcity
+        self.v = 0  # y component of velocity
+
+        self.is_sun = False
+        self.distance_from_sun = 0
 
     def __str__(self):
         return f"<{self.name.upper()}>"
